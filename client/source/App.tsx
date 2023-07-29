bb2c4b3a6644cca0aa4b428ceeb0f79364bb83fb
viral32111 <19510403+viral32111@users.noreply.github.com>
2023-07-29 22:10:40 +0100

    Rename Vite source directory for consistency

--- client/source/App.tsx
+++ client/source/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/public/vite.svg'
 import './App.css'
 
 function App() {
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>source/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
