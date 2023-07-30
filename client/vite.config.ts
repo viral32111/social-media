c66fa86c09a8212c34300a498fc34526a52110b1
viral32111 <19510403+viral32111@users.noreply.github.com>
2023-07-30 13:27:32 +0100

    Try keep assets in same directory to fix GitHub Pages

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,11 @@ import react from "@vitejs/plugin-react-swc"
 
 // https://vitejs.dev/config/
 export default defineConfig( {
+	build: {
+		outDir: "dist",
+		assetsDir: ""
+	},
 	plugins: [
 		react()
-	],
+	]
 } )
